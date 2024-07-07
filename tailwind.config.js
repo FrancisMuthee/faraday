84c90e601a7bbad633acb9d7914789820412a84a
Francis <francisnjaramba2@gmail.com>
2024-07-07 12:14:57 +0300

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.{html,js}"],
-  theme: {
+  purge: ['./templates/**/*.html', './templates/**/*.js'],
+   theme: {
     extend: {},
   },
   plugins: [require('@tailwindcss/aspect-ratio'),],
